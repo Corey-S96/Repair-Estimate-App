0032e816f1240b69f3b1309aa3830446f6f55f8a
Corey-S96 <schleiningcorey@gmail.com>
2021-07-15 13:14:10 -0500

    Changed the UI to add parts costs to total, calculator now multiplys shop rate by # of hours then adds costs of parts to subtotal field, repair description was added for future screenshot save feature

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function multiplyBy()
+
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+// function addTogether()
+// {
+//         num3 = document.getElementById("thirdNumber").value;
+//         num4 = document.getElementById("fourthNumber").value;
+//         document.getElementById("result2").innerHTML = +num3 + +num4;
+// }
+
+
+///////
+
+function calculateTotal()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
-}
-
-function addTogether()
-{
         num3 = document.getElementById("thirdNumber").value;
-        num4 = document.getElementById("fourthNumber").value;
-        document.getElementById("result2").innerHTML = +num3 + +num4;
+        document.getElementById("result").innerHTML = num1 * num2 + +num3;
 }
