e2e4a31c0e3eb5be6a70d6745451f43ea77a2c9b
Corey-S96 <schleiningcorey@gmail.com>
2021-07-15 11:17:32 -0500

    Added calculator input fields for parts and labour costs, centered form layout

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function multiplyBy()
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
+
+function addTogether()
+{
+        num3 = document.getElementById("thirdNumber").value;
+        num4 = document.getElementById("fourthNumber").value;
+        document.getElementById("result2").innerHTML = +num3 + +num4;
+}
