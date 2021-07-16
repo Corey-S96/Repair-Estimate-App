6f6e2eeeed54e44612a246f94246176bd9e5688d
Corey-S96 <schleiningcorey@gmail.com>
2021-07-15 23:15:15 -0500

    Style Changes: Button Hover animation, gradient on form, border radius on input fields

--- script.js
+++ script.js
@@ -16,10 +16,11 @@
 
 ///////
 
-function calculateTotal()
-{
+function calculateTotal() {
+
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         num3 = document.getElementById("thirdNumber").value;
         document.getElementById("result").innerHTML = num1 * num2 + +num3;
+
 }
