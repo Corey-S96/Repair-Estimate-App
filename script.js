3623a23f20fa16db83bdfa26ea4e6499121d8c48
Corey-S96 <schleiningcorey@gmail.com>
2021-07-21 19:45:09 -0500

    changed html2canvas code to target just the form block

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-
-// function multiplyBy()
-// {
-//         num1 = document.getElementById("firstNumber").value;
-//         num2 = document.getElementById("secondNumber").value;
-//         document.getElementById("result").innerHTML = num1 * num2;
-// }
-
-// function addTogether()
-// {
-//         num3 = document.getElementById("thirdNumber").value;
-//         num4 = document.getElementById("fourthNumber").value;
-//         document.getElementById("result2").innerHTML = +num3 + +num4;
-// }
-
-
-///////
-
 function calculateTotal() {
 
         num1 = document.getElementById("firstNumber").value;
